6f9537b2c2ef84a60cc4e7a26004c08a92851bb2
Pedro Júnior <pedrojuniorifpb@gmail.com>
2022-11-07 22:46:43 -0300

    Mechendo nos botões

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,17 +2,16 @@ import React from "react";
 
 export default (props) => {
     const full = props.full || false;
-    let textClass = `text-base w-1/2 h-9 p-1 rounded-lg duration-300 
-        border-2 border-solid border-primary `
+    let textClass = `text-base w-1/2 h-9 rounded-lg duration-300 `
 
     if (full) {
-        textClass += "bg-primary text-white hover:bg-primary-dark"
-        
+        textClass += `bg-primary text-white hover:bg-primary-dark`
+
         return <button className={textClass}>{props.text}</button>
     }
 
-    textClass += `bg-transparent text-primary hover:bg-white-100 
-        flex justify-center`
+    textClass += `bg-transparent text-primary hover:bg-white-100 flex 
+        justify-center items-center border-2 border-solid border-primary`
 
     return (
         <a href={props.href} className={textClass}>
