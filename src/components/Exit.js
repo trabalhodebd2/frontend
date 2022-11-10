1db5b3f0298e8802ac99fd7784940494efa16e83
Pedro Júnior <pedrojuniorifpb@gmail.com>
2022-11-10 09:45:43 -0300

    Adicionando componente Exit

---
+++ src/components/Exit.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import logOut from "../img/log-out.svg"
+
+export default () => {
+    return (
+        <a href="/login" className="flex items-center">
+            <img src={logOut} className="h-6 mr-2" />
+            <p className="text-primary text-2xl">sair</p>
+        </a>
+    )
+}
--- src/components/MenuLeft.js
+++ src/components/MenuLeft.js
@@ -2,18 +2,20 @@ import React from "react"
 
 import MenuButtons from "../components/MenuButtons"
 import Logo from "../components/Logo"
+import Exit from "../components/Exit"
 
 export default (props) => {
     return (
-        <section className="bg-white w-72 h-full">
-            <div className="p-4 pt-12 flex flex-col justify-between ">
+        <section className={`bg-white w-72 px-4 py-12 h-full 
+            flex flex-col justify-between items-center`}>
+            <div className="flex flex-col justify-between ">
                 <picture className="mb-32">
                     <Logo size="small" />
                 </picture>
                 <MenuButtons position={props.button_position} />
             </div>
 
-            {/* <Exit /> */}
+            <Exit />
         </section>
     )
 }
