7bc296a2a6c7b3241ffca222e0dd19303b7d46a3
Pedro Júnior <pedrojuniorifpb@gmail.com>
2022-11-14 15:23:54 -0300

    Evento de hover adicionado

--- src/components/UserPhoto.js
+++ src/components/UserPhoto.js
@@ -7,18 +7,26 @@ export default (props) => {
     const size = props.size || "small"
     const textClass = "rounded-full h-[3.75rem] cursor-pointer"
 
-    if (size == "small") {
+    if (size === "small") {
         return (
             <a href="account">
-                <img src={photoUserSmall} className={textClass} />
+                <img src={photoUserSmall} className={textClass} alt="Usuario" />
            </a>
         )
     }
 
     return (
-        <img 
-            src={photoUser} 
-            className={textClass + " user-photo"} 
-        />
+        <div className="relative">
+            <img 
+                src={photoUser} 
+                className={textClass + " h-[9.375rem]"} 
+                alt="Foto de Usuario"
+            />
+            <div 
+                className={`absolute h-full w-full bg-secondary text-white text-lg 
+                    underline grid place-items-center duration-300 top-0 font-bold
+                    rounded-full cursor-pointer opacity-0 hover:opacity-[85%]`}
+            >Alterar foto</div>
+        </div>
     )
 }
