283a4fcd086244383c323f0f32479fa45d805467
Pedro Júnior <pedrojuniorifpb@gmail.com>
2022-11-16 15:30:45 -0300

    Adicionando o select

---
+++ src/components/Select.js
@@ -0,0 +1,27 @@
+import React, { createElement } from "react";
+
+export default (props) => {
+    const textClass = "bg-white-100 w-full h-12 rounded-xl p-4 text-sm"
+    
+    const valueSelect = props.value
+    const listOptions = props.options
+    
+    const listElement = listOptions.map(obj => {
+        if (valueSelect === obj.value) {
+            return <option value={obj.value} selected>{obj.name}</option>
+        }
+        return <option value={obj.value}>{obj.name}</option>
+    })
+    
+    return (
+        <label className='flex flex-col w-full mt-2'>
+            <p className="text-base m-2">{props.title}</p>
+            <div className="div-select">
+                <select className={textClass}>
+                    <option disabled selected>Selecione uma das opções</option>
+                    {listElement}
+                </select>
+            </div>
+        </label>
+    )
+}
--- src/pages/Account.js
+++ src/pages/Account.js
@@ -4,10 +4,11 @@ import { isAuthenticated } from "../controller/auttentication"
 import { getValuesOfInputs } from "../controller/elements"
 
 import UserPhoto from "../components/UserPhoto"
+import DivButton from "../components/DivButton"
 import MenuLeft from "../components/MenuLeft"
+import Select from "../components/Select"
 import Search from "../components/Search"
 import Input from "../components/Input"
-import DivButton from "../components/DivButton"
 import Form from "../components/Form"
 
 export default () => {
@@ -44,7 +45,14 @@ export default () => {
                         <section className="grid grid-cols-2 grid-rows-3 gap-x-8 gap-y-4">
                             <Input title="Nome" value="José da Silva Júnior" />
                             <Input title="email" type="email" value="jose@gmail.com" />
-                            <Input title="Sexo" type="option" value="M" />
+                            <Select 
+                                title="Sexo" 
+                                options={[
+                                    {name: "Masculino", value: "M"}, 
+                                    {name: "Feminino", value: "F"}
+                                ]} 
+                                value="M"
+                            />
                             <Input title="Data de nascimento" type="date" value="2002-05-21" />
                             <Input title="Número do Telefone" value="+55 (83) 99101-7224" />
                             <Input title="Senha" type="password" value="************" />
